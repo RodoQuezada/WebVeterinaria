55ad47d86bd95447dd48fdd2babed9be56db12a3
Rodolfo Quezada <rodolfo.quezada.s@gmail.com>
2018-01-08 00:49:58 -0300

    Crear cliente completo.
    Se complementa persistencia de modulo  Cliente, crear cliente.

---
+++ src/main/java/cl/gazulabs/controller/CrearClienteController.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cl.gazulabs.controller;
+
+import cl.gazulabs.ejb.PersonaFacadeLocal;
+import cl.gazulabs.model.Persona;
+import java.io.Serializable;
+import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+/**
+ *
+ * @author rodo
+ */
+@Named
+@ViewScoped
+public class CrearClienteController implements Serializable{
+    @EJB
+    private PersonaFacadeLocal personaFacade;
+    
+    @Inject
+    private Persona persona;
+
+    public void registrar(){
+        try {
+            persona.setCodigo(persona.getRut());
+            personaFacade.create(persona);
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Aviso", "Se a registrado de forma correcta "));
+        } catch (Exception e) {
+           FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Aviso", "Error al registrar"));       
+        }
+    }
+    
+    
+    public Persona getPersona() {
+        return persona;
+    }
+
+    public void setPersona(Persona persona) {
+        this.persona = persona;
+    }
+    
+    
+    
+}
