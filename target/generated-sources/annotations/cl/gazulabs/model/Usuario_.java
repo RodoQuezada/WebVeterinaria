0e79f4fe5b282c7e90ee063fcf5d0b53331422c6
Rodolfo Quezada <rodolfo.quezada.s@gmail.com>
2018-01-14 04:07:43 -0300

    cambios
    no me acuerdo

--- target/generated-sources/annotations/cl/gazulabs/model/Usuario_.java
+++ target/generated-sources/annotations/cl/gazulabs/model/Usuario_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-01-10T22:42:34")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-01-14T02:54:51")
 @StaticMetamodel(Usuario.class)
 public class Usuario_ { 
 
